9afa9538fa682aa6ba0fe634683e83cc6d0fba1c
Laurens Nolting <contact@laurensnol.com>
2024-02-22 22:35:43 +0100

    Fixed sanitizers on Windows/GCC

--- cmake/Sanitizers.cmake
+++ cmake/Sanitizers.cmake
@@ -12,16 +12,20 @@ elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
     message(WARNING "Skipped enabling sanitizers\n"
       "GCC does not support sanitizers on MacOS Ventura\n"
       "See: https://github.com/orgs/Homebrew/discussions/3384#discussioncomment-6264292")
+  elseif(WIN32)
+    message(WARNING "Skipped enabling sanitizers\n"
+      "MinGW GCC does not support sanitizers\n"
+      "See: https://www.mingw-w64.org/contribute/#thorough-status-report-for-sanitizers-asan-tsan-usan")
+  else()
+    set(CPPSTART_SANITIZER_FLAGS
+      "-fno-omit-frame-pointer"
+      "-fsanitize=address,undefined,leak")
+    target_compile_options(${PROJECT_NAME} PRIVATE ${CPPSTART_SANITIZER_FLAGS})
+    target_compile_options(example PRIVATE ${CPPSTART_SANITIZER_FLAGS})
+
+    target_link_options(${PROJECT_NAME} PRIVATE "-fsanitize=address,undefined,leak")
+    target_link_options(example PRIVATE "-fsanitize=address,undefined,leak")
   endif()
-
-  set(CPPSTART_SANITIZER_FLAGS
-    "-fno-omit-frame-pointer"
-    "-fsanitize=address,undefined,leak")
-  target_compile_options(${PROJECT_NAME} PRIVATE ${CPPSTART_SANITIZER_FLAGS})
-  target_compile_options(example PRIVATE ${CPPSTART_SANITIZER_FLAGS})
-
-  target_link_options(${PROJECT_NAME} PRIVATE "-fsanitize=address,undefined,leak")
-  target_link_options(example PRIVATE "-fsanitize=address,undefined,leak")
 elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
   target_compile_options(${PROJECT_NAME} PRIVATE "/fsanitize=address")
   target_compile_options(example PRIVATE "/fsanitize=address")
