c29702fd401acfc457279da08b49e43c0f1cc62f
Laurens Nolting <contact@laurensnol.com>
2024-02-23 21:28:58 +0100

    Refactored CMake files

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -1,13 +1,27 @@
-if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-  target_compile_options(${PROJECT_NAME} PRIVATE
-    "-fprofile-instr-generate"
-    "-fcoverage-mapping")
-  target_link_libraries(${PROJECT_NAME} PRIVATE "-fprofile-instr-generate")
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-  target_compile_options(${PROJECT_NAME} PRIVATE "--coverage")
-  target_link_libraries(${PROJECT_NAME} PRIVATE "--coverage")
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-  message(STATUS "Skipped setting up coverage for MSVC")
-else()
-  message(WARNING "Skipped setting up coverage for unknown compiler ${CMAKE_CXX_COMPILER_ID}")
-endif()
+function(cppstart_enable_coverage)
+  message(STATUS "Enabling coverage for target(s) ${ARGN}")
+
+  set(CPPSTART_COVERAGE_COMPILER_FLAGS "")
+  set(CPPSTART_COVERAGE_LINK_LIBRARIES "")
+
+  if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    set(CPPSTART_COVERAGE_COMPILER_FLAGS
+      "-fprofile-instr-generate"
+      "-fcoverage-mapping")
+    set(CPPSTART_COVERAGE_LINK_LIBRARIES  "-fprofile-instr-generate")
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    set(CPPSTART_COVERAGE_COMPILER_FLAGS "--coverage")
+    set(CPPSTART_COVERAGE_LINK_LIBRARIES "--coverage")
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+    message(STATUS "Skipped setting up coverage for MSVC")
+    return()
+  else()
+    message(WARNING "Skipped setting up coverage for unknown compiler ${CMAKE_CXX_COMPILER_ID}")
+    return()
+  endif()
+
+  foreach(target ${ARGN})
+    target_compile_options(${target} PRIVATE ${CPPSTART_COVERAGE_COMPILER_FLAGS})
+    target_link_libraries(${target} PRIVATE ${CPPSTART_COVERAGE_LINK_LIBRARIES})
+  endforeach()
+endfunction()
--- cmake/Tooling.cmake
+++ cmake/Tooling.cmake
@@ -1,57 +1,64 @@
-set(CLANG_TIDY_PROPERTIES "")
-find_program(CLANG_TIDY_PROGRAM clang-tidy)
-if(CLANG_TIDY_PROGRAM)
-  list(APPEND CLANG_TIDY_PROPERTIES
-    ${CLANG_TIDY_PROGRAM}
-    "-p=${PROJECT_BINARY_DIR}"
-    "-header-filter=.*"
-    "--warnings-as-errors=*")
-  message(STATUS "Enabled clang-tidy")
-else()
-  message(STATUS "clang-tidy not found")
-endif()
+function(cppstart_enable_tooling)
+  message(STATUS "Enabling tooling for targets ${ARGN}")
 
-set(CPPCHECK_PROPERTIES "")
-find_program(CPPCHECK_PROGRAM cppcheck)
-if(CPPCHECK_PROGRAM)
-  # Suppress "unusedFunction" here to avoid false positives since cppcheck
-  # will check every file individually. Truly unused functions will be caught
-  # by clang-tidy.
-  list(APPEND CPPCHECK_PROPERTIES
-    ${CPPCHECK_PROGRAM}
-    "--quiet"
-    "--enable=all"
-    "--suppress=missingIncludeSystem"
-    "--suppress=unusedFunction"
-    "--suppress=unmatchedSuppression"
-    "--suppress=checkersReport"
-    "--inline-suppr")
-  message(STATUS "Enabled cppcheck")
-else()
-  message(STATUS "cppcheck not found")
-endif()
+  set(CPPSTART_CLANG_TIDY_PROPERTIES "")
+  find_program(CLANG_TIDY_PROGRAM clang-tidy)
+  if(CLANG_TIDY_PROGRAM)
+    list(APPEND CPPSTART_CLANG_TIDY_PROPERTIES
+      ${CLANG_TIDY_PROGRAM}
+      "-p=${PROJECT_BINARY_DIR}"
+      "-header-filter=.*"
+      "--warnings-as-errors=*")
+    message(STATUS "Enabled clang-tidy")
+  else()
+    message(STATUS "clang-tidy not found")
+  endif()
 
-set(IWYU_PROPERTIES "")
-find_program(IWYU_PROGRAM include-what-you-use)
-if(IWYU_PROGRAM)
-  set(IWYU_PROPERTIES "${IWYU_PROGRAM};-Xiwyu;")
-  message(STATUS "Enabled include-what-you-use")
-else()
-  message(STATUS "include-what-you-use not found")
-endif()
+  set(CPPSTART_CPPCHECK_PROPERTIES "")
+  find_program(CPPCHECK_PROGRAM cppcheck)
+  if(CPPCHECK_PROGRAM)
+    # Suppress "unusedFunction" here to avoid false positives since cppcheck
+    # will check every file individually. Truly unused functions will be caught
+    # by clang-tidy.
+    list(APPEND CPPSTART_CPPCHECK_PROPERTIES
+      ${CPPCHECK_PROGRAM}
+      "--quiet"
+      "--enable=all"
+      "--suppress=missingIncludeSystem"
+      "--suppress=unusedFunction"
+      "--suppress=unmatchedSuppression"
+      "--suppress=checkersReport"
+      "--inline-suppr")
+    message(STATUS "Enabled cppcheck")
+  else()
+    message(STATUS "cppcheck not found")
+  endif()
 
-set(CCACHE_PROPERTIES "")
-find_program(CCACHE_PROGRAM ccache)
-if(CCACHE_PROGRAM)
-  list(APPEND CCACHE_PROPERTIES
-    ${CCACHE_PROGRAM})
-  message(STATUS "Enabled Ccache")
-else()
-  message(STATUS "Ccache not found")
-endif()
+  set(CPPSTART_IWYU_PROPERTIES "")
+  find_program(IWYU_PROGRAM include-what-you-use)
+  if(IWYU_PROGRAM)
+    set(CPPSTART_IWYU_PROPERTIES "${IWYU_PROGRAM};-Xiwyu;")
+    message(STATUS "Enabled include-what-you-use")
+  else()
+    message(STATUS "include-what-you-use not found")
+  endif()
 
-set_target_properties(${PROJECT_NAME} example PROPERTIES
-  CXX_CLANG_TIDY           "${CLANG_TIDY_PROPERTIES}"
-  CXX_CPPCHECK             "${CPPCHECK_PROPERTIES}"
-  CXX_INCLUDE_WHAT_YOU_USE "${IWYU_PROPERTIES}"
-  CXX_COMPILER_LAUNCHER    "${CCACHE_PROPERTIES}")
+  set(CPPSTART_CCACHE_PROPERTIES "")
+  find_program(CCACHE_PROGRAM ccache)
+  if(CCACHE_PROGRAM)
+    list(APPEND CPPSTART_CCACHE_PROPERTIES
+      ${CCACHE_PROGRAM})
+    message(STATUS "Enabled Ccache")
+  else()
+    message(STATUS "Ccache not found")
+  endif()
+
+  foreach(target ${ARGN})
+    set_target_properties(${target} PROPERTIES
+      CXX_CLANG_TIDY           "${CPPSTART_CLANG_TIDY_PROPERTIES}"
+      CXX_CPPCHECK             "${CPPSTART_CPPCHECK_PROPERTIES}"
+      CXX_INCLUDE_WHAT_YOU_USE "${CPPSTART_IWYU_PROPERTIES}"
+      CXX_COMPILER_LAUNCHER    "${CPPSTART_CCACHE_PROPERTIES}")
+  endforeach()
+endfunction()
+
